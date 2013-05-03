01c8942ae7f315e437e519799ccbc615081867f9
Sym Roe <sym.roe@talusdesign.co.uk>
2013-05-03 14:53:39 +0100

    description

--- kentwellapp/areas/models.py
+++ kentwellapp/areas/models.py
@@ -2,7 +2,14 @@ from django.contrib.gis.db import models
 
 class Area(models.Model):
     name = models.CharField(blank=True, max_length=255)
+    description = models.TextField(blank=True)
     
     area = models.MultiPolygonField()
 
     objects = models.GeoManager()
+    
+    def __unicode__(self):
+        return self.name
+        
+        # ALTER TABLE areas_area
+        #    ADD COLUMN description text;
